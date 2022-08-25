d60ec6f63f12c230dc3b4cd976156611b821631a
Tom Stockwell <2060486+stocky37@users.noreply.github.com>
2022-08-25 13:33:42 +0100

    refactor with variants

--- overlays/homelab/prod/app/env.js
+++
@@ -1,3 +0,0 @@
-window.env = {
-	REACT_APP_API_URL: 'https://greeting-api.demo-gitops-cicd-prod.apps.paas.lab.stocky37.dev',
-}
---
+++ overlays/homelab/prod/files/env.js
@@ -0,0 +1,3 @@
+window.env = {
+	REACT_APP_API_URL: 'https://greeting-api.demo-gitops-cicd.apps.paas.lab.stocky37.dev',
+}
